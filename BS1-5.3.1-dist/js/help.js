1da5432c7c3564991623b5beb1a0324b1f2cd535
Randa-10 <randa010011012@gmail.com>
2023-09-01 17:56:08 +0300

    help and register and login

--- BS1-5.3.1-dist/js/help.js
+++ BS1-5.3.1-dist/js/help.js
@@ -2,13 +2,13 @@ var but2 = document.getElementById("vvv2");
 but2.addEventListener("click", function () {
     document.getElementById("head").innerHTML = "Track your package";
     document.getElementById("para").innerHTML =
-        "You can find tracking information in your order details. If an order includes multiple items, each may have separate delivery dates and tracking."
+    "If your tracking information shows that your package was delivered, but you haven't yet received it, wait until the end of the day. Some couriers may deliver until 10pm."
     document.getElementById("head2").innerHTML = "Find a missing package that shows As delivered";
     document.getElementById("para2").innerHTML =
         "If your tracking information shows that your package was delivered, but you haven't yet received it, wait until the end of the day. Some couriers may deliver until 10pm."
     document.getElementById("head3").innerHTML = "Undeliverable Packages";
     document.getElementById("para3").innerHTML =
-        " Occasionally, packages are returned to us as undeliverable."
+        " You can find tracking information in your order details. If an order includes multiple items, each may have separate delivery dates and tracking."
     document.getElementById("div4").style.display = "none";
     document.getElementById("div5").style.display = "none";
     document.getElementById("div6").style.display = "none";
@@ -29,8 +29,8 @@ but3.addEventListener("click", function () {
     "You can find tracking information in your order details. If an order includes multiple items, each may have separate delivery dates and tracking."
     document.getElementById("head3").innerHTML = "Undeliverable Packages";
     document.getElementById("para3").innerHTML =
-        " Occasionally, packages are returned to us as undeliverable."
-    document.getElementById("div4").style.display = "block";
+    "You can find tracking information in your order details. If an order includes multiple items, each may have separate delivery dates and tracking." 
+       document.getElementById("div4").style.display = "block";
     document.getElementById("div5").style.display = "block";
     document.getElementById("div6").style.display = "block";
     document.getElementById("div7").style.display = "block";
@@ -45,13 +45,13 @@ var but4 = document.getElementById("vvv4");
 but4.addEventListener("click", function () {
     document.getElementById("head").innerHTML = "Track your package";
     document.getElementById("para").innerHTML =
-        "You can find tracking information in your order details. If an order includes multiple items, each may have separate delivery dates and tracking."
+    "If your tracking information shows that your package was delivered, but you haven't yet received it, wait until the end of the day. Some couriers may deliver until 10pm."
     document.getElementById("head2").innerHTML = "Find a missing package that shows As delivered";
     document.getElementById("para2").innerHTML =
         "If your tracking information shows that your package was delivered, but you haven't yet received it, wait until the end of the day. Some couriers may deliver until 10pm."
     document.getElementById("head3").innerHTML = "Undeliverable Packages";
     document.getElementById("para3").innerHTML =
-        " Occasionally, packages are returned to us as undeliverable."
+        " Occasionally, packages are returned to us as undeliverable. Occasionally, packages are returned to us as undeliverable."
     document.getElementById("div4").style.display = "block";
     document.getElementById("div5").style.display = "block";
     document.getElementById("div6").style.display = "none";
@@ -64,7 +64,8 @@ but4.addEventListener("click", function () {
 var but5 = document.getElementById("vvv5");
 but5.addEventListener("click", function () {
     document.getElementById("head").innerHTML = "Placing orders";
-    document.getElementById("para").innerHTML = "Placing orders."
+    document.getElementById("para").innerHTML =         "Marketplace sellers are independent sellers who offer a variety of products and goods.."
+
     document.getElementById("head2").innerHTML = "Ordering From Marketplace Sellers";
     document.getElementById("para2").innerHTML =
         "Marketplace sellers are independent sellers who offer a variety of products and goods.."
